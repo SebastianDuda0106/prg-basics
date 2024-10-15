b55c164a7d3aa31052fbc4d48bd69a719c861342
Sebastian Duda <sebastian777duda@outlook.com>
2024-10-15 11:59:15 +0200

    sekcja 5 rozdziału 3 skończona

--- 03-ControlStructures/programs/5.8.py
+++ 03-ControlStructures/programs/5.8.py
@@ -1,6 +1,9 @@
 ###
 # ATM (cash machine) simulator
 #
+
+import math
+
 balance = 1000  # Initial balance
 pin = '1111' # initial 4-digit PIN code
 
@@ -10,30 +13,35 @@ while True:
     print()
     print("ATM Menu:")
     print("1. Check balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Change pin")
-    print("5. Exit")
+    print("2. Check pin")
+    print("3. Deposit")
+    print("4. Withdraw")
+    print("5. Change pin")
+    print("6. Exit")
 
-    choice = input("Choose an option (1-5): ")
+    choice = input("Choose an option (1-6): ")
     print()
 
     if choice == '1':
         print(f"Your current balance is: €{balance}")
     elif choice == '2':
+        print(f"Your current pin is: {pin}")
+    elif choice == '3':
         amount = float(input("Enter the amount to deposit: "))
         balance += amount
         print(f"€{amount} has been deposited. New balance: €{balance}")
-    elif choice == '3':
+    elif choice == '4':
         amount = float(input("Enter the amount to withdraw: "))
         if amount <= balance:
             balance -= amount
             print(f"€{amount} has been withdrawn. New balance: €{balance}")
         else:
             print("Insufficient balance.")
-    elif choice == '4':
-        pin=int(input('put in new pin: '))
     elif choice == '5':
+        pin=input('put in new pin: ')
+        while not pin.isdigit() or len(pin) !=4:
+            pin=input('put in new pin: ')
+    elif choice == '6':
         print("Exiting... Thank you for using the ATM!")
         break  # Exit the loop
     else:
